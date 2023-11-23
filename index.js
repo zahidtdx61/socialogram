abbe4471029323af003ce560e2946cdc87f0ce30
zahidtdx61 <45038726+zahidtdx61@users.noreply.github.com>
2023-11-24 01:36:49 +0600

    Add ejs as  view engine

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = express();
 //use express router
 app.use('/', require('./routes'));
 
-app.listen(port, function(err){
-  if(err){
-    console.log(`Error in running server: ${err}`);
-  }
+// adding veiw engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-  console.log(`Server is running on port: ${port}`);
+app.listen(port, function (err) {
+    if (err) {
+        console.log(`Error in running server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
 });
