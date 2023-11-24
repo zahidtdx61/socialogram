48e55b97d55d8ac41d4100b0c6bbdc3aa47ae50f
zahidtdx61 <45038726+zahidtdx61@users.noreply.github.com>
2023-11-24 16:45:57 +0600

    adding express layout

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const cookieParse = require('cookie-parser');
+const expressLayouts = require('express-ejs-layouts');
 const port = 8000;
 const app = express();
+
+// define static folder
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// body parser
+app.use(express.urlencoded());
+app.use(cookieParse());
 
 //use express router
 app.use('/', require('./routes'));
