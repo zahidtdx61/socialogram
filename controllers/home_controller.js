0048345636ee0e20e15da89f0263923c2b2ca9bb
zahidtdx61 <45038726+zahidtdx61@users.noreply.github.com>
2023-11-30 13:19:48 +0600

    added auth on posting content

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,20 @@
+const Post = require('../models/post');
+
 module.exports.home = function (req, res) {
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: 'Home'
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    Post.find({}).populate('user')
+        .then((posts) => {
+            return res.render('home', {
+                title: 'Socialogram | Home',
+                posts: posts
+            });
+        })
+        .catch((err) => {
+            if (err) {
+                console.log('Somthing went wrong. Not getting details. - Homepage');
+                return;
+            }
+        });
 }
