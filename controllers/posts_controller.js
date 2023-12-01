03c940a050b90e27bb6a868d366f371fd6c095c9
zahidtdx61 <45038726+zahidtdx61@users.noreply.github.com>
2023-12-01 23:42:30 +0600

    added post deletion

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function (req, res) {
     Post.create({
@@ -9,6 +10,34 @@ module.exports.create = function (req, res) {
             if (err) { console.log('Error in creating post!!!'); return; }
         })
         .then((post) => {
+            return res.redirect('back');
+        });
+}
+
+module.exports.destroy = function (req, res) {
+    Post.findById(req.params.id)
+        .then((post) => {
+            if(post.user == req.user.id){
+                Post.findByIdAndDelete(post._id)
+                    .catch((err) => {
+                        console.log(`Something went wrong during deleting post !!!: ${err}`);
+                        return res.redirect('back');
+                    });
+
+                Comment.deleteMany({post: req.params.id})
+                    .then(()=>{
+                        return res.redirect('back');
+                    })
+                    .catch((err) => {
+                        console.log(`Something went wrong during deleting all comments of a post !!!: ${err}`);
+                        return res.redirect('back');
+                    });
+            }else{
+                return res.redirect('back');
+            }
+        })
+        .catch((err) => {
+            console.log(`Something went wrong during deleting post !!!: ${err}`);
             return res.redirect('back');
         });
 }
