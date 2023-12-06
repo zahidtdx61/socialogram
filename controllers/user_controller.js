c2ac636a10a27c33978f30a8cb6179db6dbb8d1a
zahidtdx61 <45038726+zahidtdx61@users.noreply.github.com>
2023-12-06 11:43:06 +0600

    updating unauthorization

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -6,15 +6,15 @@ const User = require('../models/user');
 module.exports.profile = async function (req, res) {
     try {
         let userId = req.params.id;
-        let user = await  User.findById(userId);
+        let user = await User.findById(userId);
 
         return res.render('user_profile', {
             title: 'Profile',
             userProfile: user
-        });  
+        });
     } catch (err) {
         console.log(`Error : ${err}`);
-        return res.status(404).render('404_not_found', {title: 'Not Found'});
+        return res.status(404).render('404_not_found', { title: 'Not Found' });
     }
 }
 
@@ -41,48 +41,42 @@ module.exports.signIn = function (req, res) {
 }
 
 // get sign up data
-module.exports.create = function (req, res) {
-    if (req.body.password != req.body.confirm_password) {
+module.exports.create = async function (req, res) {
+    try {
+        if (req.body.password == req.body.confirm_password) {
+            let user = await User.findOne({ email: req.body.email });
+
+            if (!user) {
+                await User.create(req.body);
+                return res.redirect('/user/sign-in');
+            } else {
+                return res.redirect('back');
+            }
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log(`Error : ${err}`);
         return res.redirect('back');
-    } else {
-        User.findOne({ email: req.body.email })
-            .catch((err) => {
-                if (err) { console.log('Error in user sign up. Please try later.'); return; }
-            })
-            .then((user) => {
-                if (!user) {
-                    User.create(req.body)
-                        .catch((err) => {
-                            if (err) { console.log('Error in user sign up. Please try later.'); return; }
-                        })
-                        .then((user) => {
-                            return res.redirect('/user/sign-in')
-                        });
-                } else {
-                    return res.redirect('back');
-                }
-            });
     }
 }
 
 // update user data
-module.exports.update = function (req, res) {
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, {
-            name: req.body.name,
-            email: req.body.email
-        })
-            .then((err) => {
-                return res.redirect('back');
-            })
-            .catch((err) => {
-                if(err){
-                    console.log(`Something went wrong during updating user : ${err}`);
-                    return res.redirect('back');
-                }
-            })
-    }else{
-        return res.status(401).send('Unauthorized');
+module.exports.update = async function (req, res) {
+    try {
+        if (req.user.id == req.params.id) {
+            await User.findByIdAndUpdate(req.params.id, {
+                name: req.body.name,
+                email: req.body.email
+            });
+
+            return res.redirect('back');
+        } else {
+            return res.status(401).render('unauthorized', { title: 'Unauthorized' });
+        }
+    } catch (err) {
+        console.log(`Error : ${err}`);
+        return res.redirect('back');
     }
 }
 
